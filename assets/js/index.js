99ca627c8bd167678586eb93d6a35fa5d6d6a911
JackPu <kakashjack@gmail.com>
2016-02-26 11:51:58 +0800

    add files

--- assets/js/index.js
+++ assets/js/index.js
@@ -54,8 +54,11 @@
 
     };
     
+   hljs.initHighlightingOnLoad();
+    return;
     // new SliderFx view http://tympanus.net/codrops/2014/10/24/wobbly-slideshow-effect/
     new SliderFx( document.getElementById('slideshow'), {
         easing : 'cubic-bezier(.8,0,.2,1)'
     } );
+    
 })(jQuery);
