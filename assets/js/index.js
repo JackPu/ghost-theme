0aa6b9d85f9d4b368f16388329fd56927c5574b6
JackPu <kakashjack@gmail.com>
2017-03-25 23:24:37 +0800

    add new new js

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,70 +3,123 @@
  */
 
 /* globals jQuery, document */
-(function ($, undefined) {
+(function ($) {
     "use strict";
-
-    var $document = $(document);
+   
+    var animEndEventNames = {
+        'WebkitAnimation': 'webkitAnimationEnd',
+        'OAnimation': 'oAnimationEnd',
+        'msAnimation': 'MSAnimationEnd',
+        'animation': 'animationend'
+    },
+    animEndEventName = animEndEventNames[Modernizr.prefixed('animation')],
+    onEndAnimation = function (el, callback) {
+        var onEndCallbackFn = function (ev) {
+            if (ev.target != this) {
+                return ;
+            }
+            this.removeEventListener(animEndEventName, onEndCallbackFn);
+            if (callback && typeof callback === 'function') {
+                callback.call();
+            }
+        };
+        el.addEventListener(animEndEventName, onEndCallbackFn);
+    };
+    var $document = $(document),
+        containers = [].slice.call($('.slide')),
+        containersCount = containers.length,
+        current = 0,
+        isAnimating = false,
+        pageTriggers = [];
 
     $document.ready(function () {
 
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
-
-        $(".scroll-down").arctic_scroll();
-
-        $(".menu-icon, .nav-cover, .nav-close").on("click", function(e){
-            e.preventDefault();
-            if(!/iPhone|iPad|iPod|android/i.test(navigator.userAgent)) {
-                return;
-            }
-            
-            
-            
-            $("body").toggleClass("nav-opened nav-closed");
+        $('.menu-icon').on('click', function (e) {
+            $('body').toggleClass('open');
+            $(this).toggleClass('open');
         });
+        initSlider();
+
+
 
     });
-
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
-        });
-
-    };
     
-   hljs.initHighlightingOnLoad();
-    // new SliderFx view http://tympanus.net/codrops/2014/10/24/wobbly-slideshow-effect/
-    if(document.getElementById('slideshow')){
-        new SliderFx( document.getElementById('slideshow'), {
-            easing : 'cubic-bezier(.8,0,.2,1)'
-        } );
+    document.addEventListener('keydown', function (ev) {
+        var keyCode = ev.keyCode || ev.which,
+            isNavOpen = $('body').hasClass('open');
+
+        switch (keyCode) {
+            // left key
+        case 37:
+            if (current > 0 && !isNavOpen) {
+                navigate(pageTriggers[current - 1]);
+            }
+            break;
+            // right key
+        case 39:
+            if (current < containersCount - 1 && !isNavOpen) {
+                navigate(pageTriggers[current + 1]);
+            }
+            break;
+        }
+    });
+    
+    function initSlider() {
+        var thumbString = '';
+        containers.forEach(function(item, index) {
+            thumbString += '<a href="javascript:;" ' + (index === 0? 'class="active" ': '')  + '></a>;'   
+        });
+        if(containers.length > 1) {
+            var ctrlString = '<div class="ctrl"><a href="javascript:;" class="js-ctrl-left" ><svg class="icon icon-rewind"><use xlink:href="#icon-rewind"></use></svg></a><a href="javascript:;" class="js-ctrl-right icon-angle-right" ><svg class="icon icon-fast-forward"><use xlink:href="#icon-fast-forward"></use></svg></a></div>';
+            $('#slideshow').append([ctrlString, '<div class="thumb">', thumbString, '</div>'].join(''));
+            bindEvent();
+        }
+        pageTriggers = [].slice.call($('.thumb a'));
+        classie.add( containers[ current ], 'slider--current' );
+        
+        
+        
     }
     
+    function bindEvent() {
+        $('.js-ctrl-left').on('click', function() {
+             if (current > 0) {
+                navigate(pageTriggers[current - 1]);
+            }    
+        });
+        $('.js-ctrl-right').on('click', function() {
+             if (current < containersCount - 1) {
+                navigate(pageTriggers[current + 1]);
+            }
+        })
+    }
     
+    function navigate(pageTrigger) {
+        var oldcurrent = current,
+            newcurrent = pageTriggers.indexOf(pageTrigger);
+        if (isAnimating || oldcurrent === newcurrent) return;
+        isAnimating = true;
+
+        var currentPageTrigger = pageTriggers[current],
+            nextContainer = containers[newcurrent],
+            currentContainer = containers[current],
+            dir = newcurrent > oldcurrent ? 'left' : 'right';
+        
+        $(currentPageTrigger).removeClass('active');
+        $(pageTrigger).addClass('active');
+        // update current
+        current = newcurrent;
+        $(nextContainer).addClass(dir === 'left' ? 'slider--animInRight' : 'slider--animInLeft');
+        $(currentContainer).addClass(dir === 'left' ? 'slider--animOutLeft' : 'slider--animOutRight');
+        onEndAnimation(currentContainer, function () {
+            $(currentContainer).removeClass(dir === 'left' ? 'slider--animOutLeft' : 'slider--animOutRight');
+            $(nextContainer).removeClass(dir === 'left' ? 'slider--animInRight' : 'slider--animInLeft');
+            $(currentContainer).removeClass('slider--current');
+            $(nextContainer).addClass('slider--current');
+            isAnimating = false;
+        });
+    }
+
+
 })(jQuery);
+
