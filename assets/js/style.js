977cbe218d797673533ff3eb8ccc15043199598d
JackPu <kakashjack@gmail.com>
2019-09-27 15:12:11 +0800

    feat: update new tag and post page design

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,13 +4,19 @@ function addStyleString(str) {
   document.head.appendChild(node);
 }
 function generateColorCss() {
-  var cssStr;
+  var cssStr = '';
   for (var i = 0; i < 10; i++) {
-    var r = parseInt(255 * Math.random());
-    var g = parseInt(255 * Math.random());
-    var b = parseInt(255 * Math.random());
-    var color = 'rgb(' + [r, g ,b].join(',') + ')';
-    cssStr += '#content article:nth-child(10n + ' + (i+1) + ') {background-color:' + color + '}';
+      var r = parseInt(255 * Math.random());
+      var g = parseInt(255 * Math.random());
+      var b = parseInt(255 * Math.random());
+      var color = 'rgb(' + [r, g ,b].join(',') + ')';
+      cssStr += '#content article:nth-child(10n + ' + (i+1) + ') .cover {background-color:' + color + '}';
+      r = 50 + parseInt(100 * Math.random());
+      g = 30 + parseInt(50 * Math.random());
+      b = 120 +parseInt(135 * Math.random());
+      color = 'rgb(' + [r, g ,b].join(',') + ')';
+      cssStr += '#slideshow .slide:nth-child(10n + ' + (i+1) + ') {background-color:' + color + '}';
   }
   addStyleString(cssStr);
 }
+generateColorCss();
